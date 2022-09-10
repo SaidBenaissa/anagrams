d888a2c97708c3e4751172319c790caa8169a3b8
Said BENAISSA <97518723+SaidBenaissa@users.noreply.github.com>
2022-09-11 00:02:17 +0200

    Update Anagrams.py

--- Anagrams.py
+++ Anagrams.py
@@ -3,29 +3,15 @@ def make_hash(v):
     hashed = sum((hash(x) for x in list(v)))
     return hashed
 
-
 # Select all words from txt file, remove empty space
 processed_words = []
 
 
 def cleanup(contents):
     splited_contents = contents.split()
-    # ############### splited_contents ###############################################################
-    # ['akte', 'aldri', 'alle', 'aller', 'allfarveien', 'allting', 'alt', 'altfor', 'andre',......]
     for word in splited_contents:
-        # ############ word ##########################################################################
-        # dratt
-        # dreper
-        # drev
-        # drikke
-        # drikker
+
         letters = word.split()
-        # ############ letters ######################################################################
-        # ['dratt']
-        # ['dreper']
-        # ['drev']
-        # ['drikke']
-        # ['drikker']
 
         clean_word = ""
         for letter in letters:
@@ -33,8 +19,6 @@ def cleanup(contents):
             if letter.isalpha():
                 clean_word += letter
                 processed_words.append("".join(clean_word))
-                # ############ processed_words #################################################################
-                # [ler', 'talte', 'tar', 'taska', 'tatt', 'tau', 'tauet', 'tenk', 'tenke', 'tenker', 'tenkte'
 
     processed_words_frequency = {}
     for word in processed_words:
@@ -42,22 +26,13 @@ def cleanup(contents):
             processed_words_frequency[word] += 1
         else:
             processed_words_frequency[word] = 1
-    # ############ processed_words_frequency #############################################################
-    # {'akte': 1, 'aldri': 1, 'alle': 1, 'aller': 1, 'allfarveien': 1, 'allting': 1, 'alt': 1, 'altfor': 1,
-    # ...}
+    
     return processed_words_frequency
-
 
 # Read txt file to dictionary
 f = open('ordbok-utf8-2.txt', 'r', errors='ignore')
 input_data = f.read()
 clean_words = cleanup(input_data)
-# print("Number of Words in txt input file = {}".format(len(clean_words)))
-# clean_words ###############################################################################################
-# {'akte': 1, 'aldri': 1, 'alle': 1, 'aller': 1, 'allfarveien': 1, 'allting': 1, 'alt': 1, 'altfor': 1,
-# 'andre': 1, 'annen': 1, 'annet': 1,
-# ...}
-
 
 def find_anagrams(clean_words):
     # Pairing Anagrams by Hash value [hash_key: value]
@@ -71,9 +46,6 @@ def find_anagrams(clean_words):
             pass
         else:
             clean_words_hash_table[make_hash(word)].extend([word])
-    # print(clean_words_hash_table)
-    # clean_words_hash_table ############################################################
-    # {10537339097141344516: ['akte'], 28998290657399899697: ['aldri'], 16787045441859543998: ['alle'],
 
     # Detect anagrams and Clean clean_words_hash_table, only contains 2 letter or more values can be anagrams
     anagram_hash_table = {}
@@ -81,24 +53,13 @@ def find_anagrams(clean_words):
     for k, v in clean_words_hash_table.items():
         if len(v) > 1:
             anagram_hash_table[k] = v
-    # anagram_hash_table ##########################################################
-    # {10939202460068486256: ['at', 'ta'], 17281837970771535480: ['bar', 'bra'],
-    # 12315734925037834454: ['bry', 'byr'],
 
-    # Creates Hash values for all keys in clean_word
-    # clean_words ###############################################################################################
-    # {'akte': 1, 'aldri': 1, 'alle': 1, 'aller': 1, 'allfarveien': 1, 'allting': 1, 'alt': 1, 'altfor': 1,
-    # 'andre': 1, 'annen': 1, 'annet': 1,
-    # ...}
     hash_word = {}
     for k, v in clean_words.items():
         if make_hash(k) not in hash_word:
             hash_word[make_hash(k)] = v
         else:
             hash_word[make_hash(k)] += v
-    # hash_word ##########################################################
-    # {18850452034543600945: 1, -17207395831741420928: 1, 6644983032283399904: 1, 2580367764680674904: 1,
-    # ...}
 
     # Match hash_word frequency
     anagram_frequency_table = {}
